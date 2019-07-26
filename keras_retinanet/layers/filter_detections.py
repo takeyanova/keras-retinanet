5103def251d9335f4fa64164fb5bf6c4ed013210
takeyanova <51348699+takeyanova@users.noreply.github.com>
2019-07-26 14:07:07 +0800

    Update filter_detections.py

--- keras_retinanet/layers/filter_detections.py
+++ keras_retinanet/layers/filter_detections.py
@@ -16,71 +16,7 @@ limitations under the License.
 
 import keras
 from .. import backend
-import numpy as np
-import tensorflow as tf
-from keras import backend as K
 
-
-def py_soft_nms(dets,scores, method='gaussian', iou_thr=0.3, sigma=0.5, score_thr=0.001):
-    """Pure python implementation of soft NMS as described in the paper
-    `Improving Object Detection With One Line of Code`_.
-    Args:
-        dets (numpy.array): Detection results with shape `(num, 5)`,
-            data in second dimension are [x1, y1, x2, y2, score] respectively.
-        method (str): Rescore method. Only can be `linear`, `gaussian`
-            or 'greedy'.
-        iou_thr (float): IOU threshold. Only work when method is `linear`
-            or 'greedy'.
-        sigma (float): Gaussian function parameter. Only work when method
-            is `gaussian`.
-        score_thr (float): Boxes that score less than the.
-    Returns:
-        numpy.array: Retained boxes.
-    .. _`Improving Object Detection With One Line of Code`:
-        https://arxiv.org/abs/1704.04503
-    """
-    if method not in ('linear', 'gaussian', 'greedy'):
-        raise ValueError('method must be linear, gaussian or greedy')
-
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-    # expand dets with areas, and the second dimension is
-    # x1, y1, x2, y2, score, area
-    dets = np.concatenate((dets, scores,areas[:, None]), axis=1)
-
-    retained_box = []
-    while dets.size > 0:
-        max_idx = np.argmax(dets[:, 4], axis=0)
-        dets[[0, max_idx], :] = dets[[max_idx, 0], :]
-        retained_box.append(dets[0, :-1])
-
-        xx1 = np.maximum(dets[0, 0], dets[1:, 0])
-        yy1 = np.maximum(dets[0, 1], dets[1:, 1])
-        xx2 = np.minimum(dets[0, 2], dets[1:, 2])
-        yy2 = np.minimum(dets[0, 3], dets[1:, 3])
-
-        w = np.maximum(xx2 - xx1 + 1, 0.0)
-        h = np.maximum(yy2 - yy1 + 1, 0.0)
-        inter = w * h
-        iou = inter / (dets[0, 5] + dets[1:, 5] - inter)
-
-        if method == 'linear':
-            weight = np.ones_like(iou)
-            weight[iou > iou_thr] -= iou[iou > iou_thr]
-        elif method == 'gaussian':
-            weight = np.exp(-(iou * iou) / sigma)
-        else:  # traditional nms
-            weight = np.ones_like(iou)
-            weight[iou > iou_thr] = 0
-
-        dets[1:, 4] *= weight
-        retained_idx = np.where(dets[1:, 4] >= score_thr)[0]
-        dets = dets[retained_idx + 1, :]
-
-    return np.vstack(retained_box)
 
 def filter_detections(
     boxes,
@@ -121,8 +57,7 @@ def filter_detections(
             filtered_scores = keras.backend.gather(scores, indices)[:, 0]
 
             # perform NMS
-            nms_indices = py_soft_nms(filtered_boxes, filtered_scores)
-
+            nms_indices = backend.non_max_suppression(filtered_boxes, filtered_scores, max_output_size=max_detections, iou_threshold=nms_threshold,soft_nms_sigma=0.5)
             # filter indices based on NMS
             indices = keras.backend.gather(indices, nms_indices)
 
