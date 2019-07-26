bc59e2570e8b03105b040d44154e880f92a81841
takeyanova <51348699+takeyanova@users.noreply.github.com>
2019-07-26 13:16:47 +0800

    Update filter_detections.py

--- keras_retinanet/layers/filter_detections.py
+++ keras_retinanet/layers/filter_detections.py
@@ -41,8 +41,7 @@ def py_soft_nms(dets,scores, method='gaussian', iou_thr=0.3, sigma=0.5, score_th
     """
     if method not in ('linear', 'gaussian', 'greedy'):
         raise ValueError('method must be linear, gaussian or greedy')
-    dets=tf.Variable(dets)
-    dets[:,4].assign(scores)
+
     x1 = dets[:, 0]
     y1 = dets[:, 1]
     x2 = dets[:, 2]
@@ -50,7 +49,7 @@ def py_soft_nms(dets,scores, method='gaussian', iou_thr=0.3, sigma=0.5, score_th
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
     # expand dets with areas, and the second dimension is
     # x1, y1, x2, y2, score, area
-    dets = np.concatenate((dets, areas[:, None]), axis=1)
+    dets = np.concatenate((dets, scores,areas[:, None]), axis=1)
 
     retained_box = []
     while dets.size > 0:
